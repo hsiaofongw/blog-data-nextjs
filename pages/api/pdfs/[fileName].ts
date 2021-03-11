0ab4371626a24fef390816b99f2594bfd5659035
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-11 23:47:06 +0800

    update

--- pages/api/pdfs/[fileName].ts
+++ pages/api/pdfs/[fileName].ts
@@ -16,7 +16,7 @@ async function requestHandler(req: NextApiRequest, res: NextApiResponse) {
     const cacheDays = 3;
     const cacheSeconds = cacheDays * 24 * 60 * 60;
 
-    // res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Type', 'application/pdf');
     // res.setHeader('Cache-Control', `public,max-age=${cacheSeconds}`);
     res.status(200).send(resource);
 }
